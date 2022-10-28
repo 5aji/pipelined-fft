0d38e0171ba94be42c8988672d4764badb927031
saji <champ189@umn.edu>
2022-10-28 14:33:09 -0500

    attempt to fix width complaints in verilator

--- rtl/cplx_mul.v
+++ rtl/cplx_mul.v
@@ -19,8 +19,8 @@ reg signed [2*WIDTH - 1:0] res_re, res_im;
 
 // the shifted 1 by fixed point -1 is for rounding.
 localparam ROUND_FACTOR = 1 << (FIXED_POINT -1);
-assign y0_re = res_re >> FIXED_POINT;
-assign y0_im = res_im >> FIXED_POINT;
+assign y0_re = res_re[FIXED_POINT + WIDTH-1:FIXED_POINT];
+assign y0_im = res_im[FIXED_POINT + WIDTH-1:FIXED_POINT];
 always @(posedge clk) begin
 	res_re <= a_re * b_re - a_im * b_im + ROUND_FACTOR;
 	res_im <= a_re * b_im + a_im * b_re + ROUND_FACTOR;
