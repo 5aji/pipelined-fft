bece8a5bcac49d0be3d42e0a381e6d5d603288f3
saji <9110284+kschamplin@users.noreply.github.com>
2022-11-02 16:27:23 -0500

    reformat cplx multiplier

--- rtl/cplx_mul.v
+++ rtl/cplx_mul.v
@@ -5,6 +5,7 @@
 // This module is a simple complex multiplier. It assumes that the toolchain
 // can synthesize 1-clock multipliers. If this doesn't work, a different
 // module will be needed instead.
+
 module cplx_mul #(
 	parameter WIDTH=16,
 	parameter FIXED_POINT=8
@@ -16,11 +17,11 @@ module cplx_mul #(
 
 reg signed [2*WIDTH - 1:0] res_re, res_im;
 
-
 // the shifted 1 by fixed point -1 is for rounding.
 localparam ROUND_FACTOR = 1 << (FIXED_POINT -1);
 assign y0_re = res_re[FIXED_POINT + WIDTH-1:FIXED_POINT];
 assign y0_im = res_im[FIXED_POINT + WIDTH-1:FIXED_POINT];
+
 always @(posedge clk) begin
 	res_re <= a_re * b_re - a_im * b_im + ROUND_FACTOR;
 	res_im <= a_re * b_im + a_im * b_re + ROUND_FACTOR;
